ee0cdba83afbf05288b8e457c612bf9e02bf4dbe
sbassi <sbassi@genesdigitales.com>
2013-04-18 02:08:48 -0300

    First Django version (raw)

---
+++ fte/fte_main/forms.py
@@ -0,0 +1,5 @@
+from django import forms
+
+class FTEMainForm(forms.Form):
+    custom_label  = forms.CharField(max_length=30, required=True)
+    infile  = forms.FileField()
---
+++ fte/fte_main/views.py
@@ -0,0 +1,18 @@
+from django.http import HttpResponse
+from fte_main.forms import FTEMainForm
+from django.shortcuts import render, redirect
+
+from fte_main.converter import Convert_fasta_header
+
+def index(request):
+    if request.method == 'POST':
+        form = FTEMainForm(request.POST, request.FILES)
+        if form.is_valid():
+            custom_label = form.cleaned_data['custom_label']
+            x = request.FILES['infile']
+            #print custom_label, x
+            c = Convert_fasta_header(x,'out.txt', custom_label)
+            c.conv_gsp()
+    else:
+        form = FTEMainForm()
+    return render(request, 'fte_init.html', {'a':2, 'form':form})
