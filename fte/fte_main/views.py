7abb56a4d26036f87dc2aabc4f682e4a9315ae57
Sebastian Bassi <sebastian.bassi@globant.com>
2013-09-07 20:15:44 -0300

    Using bootstrap

--- fte/fte_main/views.py
+++ fte/fte_main/views.py
@@ -1,19 +1,31 @@
-from django.http import HttpResponse
-from fte_main.forms import FTEMainForm
-from django.shortcuts import render, redirect
+#from django.http import HttpResponse
+#from fte_main.forms import FTEMainForm
+from django.shortcuts import render
 
 from fte_main.converter import Convert_fasta_header
 
 def index(request):
     if request.method == 'POST':
-        form = FTEMainForm(request.POST, request.FILES)
-        if form.is_valid():
-            custom_label = form.cleaned_data['custom_label']
-            x = request.FILES['infile']
+
+        #form = FTEMainForm(request.POST, request.FILES)
+        #if form.is_valid():
+        #    custom_label = form.cleaned_data['custom_label']
+        #    x = request.FILES['infile']
             #print custom_label, x
-            c = Convert_fasta_header(x,'out.txt', custom_label)
-            c.conv_gsp()
+        #print request
+
+        ct = request.POST["ct"]
+        x = request.FILES['exampleInputFile']
+        c = Convert_fasta_header(x,'out.txt', ct)
+        c.conv_gsp()
+        print "1"
+        return render(request, 'index_result.html', {'a':2})
+
     else:
+        print "2"
         form = FTEMainForm()
     #return render(request, 'fte_init.html', {'a':2, 'form':form})
-    return render(request, 'index.html', {'a':2, 'form':form})
+    #return render(request, 'index.html', {'a':2, 'form':form})
+    print "3"
+    return render(request, 'index.html', {'a':2})
+    
--- fte/urls.py
+++ fte/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls.defaults import patterns, include, url
 
+from fte_main import views
+
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
@@ -8,7 +10,8 @@ urlpatterns = patterns('',
     # Examples:
     ##url(r'^fte/', 'fte.views.home', name='home'),
     url(r'^$', include('fte_main.urls')),
-
+    url(r'^result/$', views.index, name='result'),
+	#url(r'^articles/2003/$', 'news.views.special_case_2003'),
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
